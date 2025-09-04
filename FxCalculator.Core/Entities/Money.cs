a3071ffdcf97b44fd030f36a28887efb7f56e714
isCatNull <90348097+isCatNull@users.noreply.github.com>
2025-09-04 22:52:02 +0300

    Add money and tests

---
+++ FxCalculator.Core/Entities/Money.cs
@@ -0,0 +1,12 @@
+﻿using FxCalculator.Core.Shared;
+
+namespace FxCalculator.Core.Entities;
+
+public readonly record struct Money(decimal Amount, string Currency)
+{
+    public static Result<Money> Create(string currency, decimal amount)
+    {
+        
+        return Result<Money>.Success(new Money(amount, currency));
+    }
+}
---
+++ FxCalculator.Tests/FxCalculator.Core/MoneyTests.cs
@@ -0,0 +1,61 @@
+﻿using FxCalculator.Core.Entities;
+using NUnit.Framework;
+
+namespace FxCalculator.Tests.FxCalculator.Core;
+
+[TestFixture]
+public class MoneyTests
+{
+    private const string DefaultCurrencyForTest = "DKK";
+    private const decimal DefaultAmountForTest = 100.00M;
+    
+    [TestCase("EUR", 10.00)]
+    [TestCase("GBP", 45.00)]
+    [TestCase("DKK", 10000)]
+    [TestCase("SEK", 0.00)]
+    public void Given_ValidCurrencyAmountAndCode_SuccessfullyCreatesMoney(string currency, decimal amount)
+    {
+        // Arrange & Act
+        var result = Money.Create(currency, amount);
+
+        // Assert
+        Assert.That(result.IsSuccess, Is.True);
+        Assert.That(result.Value.Amount, Is.EqualTo(amount));
+        Assert.That(result.Value.Currency, Is.EqualTo(currency));
+    }
+    
+    [TestCase("EUUUU")]
+    [TestCase("G")]
+    [TestCase("GG")]
+    public void Given_ValidInvalidCurrency_FailsToCreateMoney(string currency)
+    {
+        // Arrange & Act
+        var result = Money.Create(currency, DefaultAmountForTest);
+
+        // Assert
+        Assert.That(result.IsSuccess, Is.False);
+    }
+    
+    [TestCase(-1)]
+    [TestCase(-45.00)]
+    public void Given_NegativeAmount_FailsToCreateMoney(decimal amount)
+    {
+        // Arrange & Act
+        var result = Money.Create(DefaultCurrencyForTest, amount);
+
+        // Assert
+        Assert.That(result.IsSuccess, Is.False);
+    }
+    
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("  ")]
+    public void Given_EmptyCurrency_FailsToCreateMoney(string currency)
+    {
+        // Arrange & Act
+        var result = Money.Create(currency, DefaultAmountForTest);
+
+        // Assert
+        Assert.That(result.IsSuccess, Is.False);
+    }
+}
